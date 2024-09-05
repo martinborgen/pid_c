760c99501a9308e3608d8f0d87e22cd5f8f26899
Martin Borgén <martinb.nymail@gmail.com>
2024-09-05 18:06:42 +0200

    removed crappy overflow handling

--- src/pid_controller.c
+++ src/pid_controller.c
@@ -11,38 +11,8 @@ int32_t update_pid(pid_cont_t* cont, int32_t ref, int32_t feedback, uint32_t mil
     int32_t i_term = cont->ki * error * millisec_delta;
     int32_t d_term = cont->kd * error / millisec_delta; // TODO Check for zero division
 
-
-    // Overflow handling. Always int max? TODO: HOw to detect underflow?
-    if (error != 0 && p_term / error != cont->kp) {
-        return INT32_MAX;
-    }
-
-    if (error != 0 && i_term / error != cont->ki) {
-        return INT32_MAX;
-    }
-
-    if (error != 0 && d_term / error != cont->kd) {
-        return INT32_MAX;
-    }
-
-    if (p_term < INT32_MAX - output) {
-        output += p_term;
-    } else {
-        return INT32_MAX;
-    }
-
-    // iterm = ki * iterm old 
     output += cont->iterm;
-    // if (cont->iterm < INT32_MAX - output) {
-    // } else {
-    //     return INT32_MAX;
-    // }
-
     output += d_term;
-    // if (d_term < INT32_MAX - output) {
-    // } else {
-    //     return INT32_MAX;
-    // }
 
     cont->iterm += i_term; // TODO Handle overflow here too
     cont->time_old = millisec;
